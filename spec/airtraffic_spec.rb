fdc28180410be7dcffe2759dd772c82c5eb1f3de
NicGiles <n.r.giles1@gmail.com>
2018-12-11 14:54:36 +0000

    Created failing test for capacity message

--- spec/airtraffic_spec.rb
+++ spec/airtraffic_spec.rb
@@ -6,7 +6,7 @@ describe AirTrafficController do
   let(:weather) { double :weather }
 
   describe '#land_plane_when_safe' do
-    context 'nice weather' do
+    context 'nice weather, plane capacity' do
       before do
         allow(weather).to receive(:unsafe_weather?).and_return false
       end
@@ -15,6 +15,15 @@ describe AirTrafficController do
         expect(plane).to receive(:land_plane_when_safe)
         heathrow.land_plane_when_safe(plane)
       end
+
+      context 'prevents landing when at capacity' do
+      it "tells pilot they can't land" do
+        8.times do
+          heathrow.land_plane_when_safe(plane)
+        end
+        expect { heathrow.land_plane_when_safe(plane) }.to raise_error 'Sorry boss, no room in the inn'
+      end
     end
+  end
 end
 end
