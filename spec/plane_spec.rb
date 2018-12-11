3382669e7883c707274661a3f57c2783b0f164fc
NicGiles <n.r.giles1@gmail.com>
2018-12-11 13:54:31 +0000

    Created plane spec and tested land_plane

---
+++ spec/plane_spec.rb
@@ -0,0 +1,16 @@
+require 'plane'
+
+describe Plane do
+  subject(:plane) { described_class.new }
+  let(:hanger) { double :hanger }
+
+  describe '#land_plane' do
+it { is_expected.to respond_to :land_plane }
+end
+
+  it 'sticks landed plane in the hanger' do
+    plane.land_plane(hanger)
+    expect(plane.hanger).to eq hanger
+  end
+
+end
