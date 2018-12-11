84a785d011bd404b79c3c5d9c996833c078f0a72
NicGiles <n.r.giles1@gmail.com>
2018-12-11 17:15:53 +0000

    Created tests for airport capacity

--- spec/airtraffic_spec.rb
+++ spec/airtraffic_spec.rb
@@ -1,8 +1,8 @@
 require 'AirTrafficController'
 
 describe AirTrafficController do
-  subject(:heathrow) { described_class.new(8, weather) }
-  let(:plane) { double :plane }
+  subject(:heathrow) { described_class.new(weather, 8) }
+  let(:plane) { double :plane, land_plane: nil, take_off: nil }
   let(:weather) { double :weather }
 
   describe '#land_plane_when_safe' do
@@ -12,18 +12,23 @@ describe AirTrafficController do
       end
 
       it 'confirms plane can land' do
-        expect(plane).to receive(:land_plane_when_safe)
+        expect(plane).to receive(:land_plane)
         heathrow.land_plane_when_safe(plane)
-      end
-
-      context 'prevents landing when at capacity' do
-      it "tells pilot they can't land" do
-        8.times do
-          heathrow.land_plane_when_safe(plane)
-        end
-        expect { heathrow.land_plane_when_safe(plane) }.to raise_error 'Sorry boss, no room in the inn'
       end
     end
   end
+
+  describe '#land_plane_when_safe' do
+    context 'nice weather, plane capacity' do
+      before(:each) do
+        allow(weather).to receive(:unsafe_weather?).and_return false
+      end
+      it "tells pilot they can't land when full" do
+              8.times do
+                heathrow.land_plane_when_safe(plane)
+              end
+              expect { heathrow.land_plane_when_safe(plane) }.to raise_error 'Sorry boss, no room in the inn'
+            end
+end
 end
 end
